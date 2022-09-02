87cbf0b4666158dbbec8179bf7af9b64bb2e7a6f
James Ritchie <james@jamesritchie.co.uk>
2022-09-02 18:35:31 +0100

    Input field now working

--- pages/components/Main.tsx
+++ pages/components/Main.tsx
@@ -2,19 +2,21 @@ import { useState } from "react";
 import { GithubLogo } from "./GithubLogo";
 import { StarCount } from "./StarCount";
 
-type MainProps = {
-  onSubmit: (orgRepo: string) => void;
+type OwnerRepo = {
+  owner: string;
+  repo: string;
 };
 
-export function Main({ onSubmit }: MainProps) {
-  const [orgRepo, setOrgRepo] = useState("");
-  const error = "Oops! Something went wrong. Please try again.";
+export function Main() {
+  const [inputValue, setInputValue] = useState("");
+  const [orgRepo, setOrgRepo] = useState<OwnerRepo | null>(null);
+  const error = "Help";
 
   return (
     <main className="flex w-full flex-1 flex-col items-center justify-top p-20 text-center z-10">
       <div className="flex flex-col items-center justify-top w-full h-96  mb-10 pt-10">
-        {true ? (
-          <StarCount repo="jsonhero-web" owner="apihero-run" />
+        {orgRepo ? (
+          <StarCount repo={orgRepo.repo} owner={orgRepo.owner} />
         ) : (
           <GithubLogo />
         )}
@@ -31,26 +33,31 @@ export function Main({ onSubmit }: MainProps) {
           className="bg-slate-100 p-2 w-72 pl-3 font-mono rounded-l placeholder:text-slate-400"
           type="text"
           placeholder="Enter a repo (org/repo)"
-          value={orgRepo}
-          onChange={(e) => setOrgRepo(e.target.value)}
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
         />
         <button
           className="bg-blue-500 transition hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r"
           onClick={() => {
-            onSubmit(orgRepo);
+            const [owner, repo] = inputValue.split("/");
+            if (owner && repo) {
+              setOrgRepo({ owner, repo });
+            }
+
+            // onSubmit(orgRepo);
           }}
         >
           Get Stars
         </button>
       </div>
-      <div className="absolute bottom-0 left-0 right-0">
-        {error && (
+      {/* {status === "error" && (
+        <div className="absolute bottom-0 left-0 right-0">
           <div className="left-[calc(50%-165px)] animate-bounce absolute top-16 bg-rose-50 px-2 py-1 text-white p-2 rounded">
             <div className="h-2 w-2 bg-rose-50 absolute -top-1 left-[calc(50%-6px)] rotate-45" />
-            <p className="text-rose-500">Oops, I couldn't find that one!</p>
+            <p className="text-rose-500">Oops, {error.message}</p>
           </div>
-        )}
-      </div>
+        </div>
+      )} */}
     </main>
   );
 }
--- pages/components/StarCount.tsx
+++ pages/components/StarCount.tsx
@@ -12,22 +12,24 @@ export function StarCount({ owner, repo }: { owner: string; repo: string }) {
 
   return (
     <>
-      <div className="w-48 h-48 mb-5">
-        <Image
-          className="animate-star-animation "
-          src="/star-large.svg"
-          alt="Star"
-          width="192"
-          height="192"
-        />
-      </div>
       <div className="text-2xl font-bold font-poppins text-slate-100 mb-5">
         {status === "loading" ? (
           "Loading..."
         ) : status === "error" ? (
           <span>Something went wrong: {error.message}</span>
         ) : (
-          <h2>{data.stargazers_count} stars</h2>
+          <>
+            <div className="w-48 h-48 mb-5">
+              <Image
+                className="animate-star-animation "
+                src="/star-large.svg"
+                alt="Star"
+                width="192"
+                height="192"
+              />
+            </div>
+            <h2>{data.stargazers_count} stars</h2>
+          </>
         )}
       </div>
       <h1 className="text-xl font-poppins text-slate-100 mb-5">
