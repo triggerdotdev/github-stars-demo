da011a0f7cf8ce9e29d28f3458171d7dfb28a9b3
D-K-P <dkp.github@pm.me>
2022-09-02 09:49:18 +0100

    Added light logo and gradient to the bottom banner

--- github-stars-demo/pages/index.tsx
+++ github-stars-demo/pages/index.tsx
@@ -35,15 +35,20 @@ const Home: NextPage = () => {
         <GetStarsInput />
       </main>
 
-      <footer className="flex z-30 h-10 w-full items-center justify-center border-t bg-slate-300">
+      <footer className="flex z-30 h-10 w-full items-center justify-center bg-gradient-to-r from-[#4669E5] via-[#2B52DE] to-[#644DF5]">
         <a
-          className="flex items-center text-sm justify-center gap-2 text-slate-800"
+          className="flex items-center text-sm justify-center gap-2 text-slate-50"
           href="https://apihero.run"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by{" "}
-          <Image src="/logo.svg" alt="API Hero Logo" width={100} height={26} />
+          <Image
+            src="/logo-light.svg"
+            alt="API Hero Logo"
+            width={100}
+            height={26}
+          />
         </a>
       </footer>
     </div>
