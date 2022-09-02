44b1a299dc6c5139d8b7bd30472a1a175d5d4486
D-K-P <dkp.github@pm.me>
2022-09-02 16:38:36 +0100

    Added a star with a little animation and moved the text around

--- pages/components/StarCount.tsx
+++ pages/components/StarCount.tsx
@@ -1,5 +1,6 @@
 import { repos } from "@apihero/github";
 import { createEndpoint } from "../apihero/react";
+import Image from "next/image";
 
 const useGetRepository = createEndpoint(repos.getRepos);
 
@@ -11,17 +12,27 @@ export function StarCount({ owner, repo }: { owner: string; repo: string }) {
 
   return (
     <>
-      <h1 className="text-2xl font-bold font-poppins text-slate-100 mb-5">
+      <div className="w-48 h-48 mb-5">
+        <Image
+          className="animate-star-animation "
+          src="/star-large.svg"
+          alt="Star"
+          width="192"
+          height="192"
+        />
+      </div>
+      <div className="text-2xl font-bold font-poppins text-slate-100 mb-5">
+        {status === "loading" ? (
+          "Loading..."
+        ) : status === "error" ? (
+          <span>Something went wrong: {error.message}</span>
+        ) : (
+          <h2>{data.stargazers_count} stars</h2>
+        )}
+      </div>
+      <h1 className="text-xl font-poppins text-slate-100 mb-5">
         {owner}/{repo}
       </h1>
-      <div className="w-48 h-48 bg-black z-50 mb-5"></div>
-      {status === "loading" ? (
-        "Loading..."
-      ) : status === "error" ? (
-        <span>Something went wrong: {error.message}</span>
-      ) : (
-        <h2>{data.stargazers_count} stars</h2>
-      )}
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,21 @@ module.exports = {
       dropShadow: {
         '2xl': '0 0 5px rgba(211, 172, 83, 0.8)',
     },
+    keyframes: {
+        starAnimation: {
+          '0%': { transform: 'rotate(0.0deg)' },
+          '10%': { transform: 'rotate(4deg)' },
+          '20%': { transform: 'rotate(-4deg)' },
+          '30%': { transform: 'rotate(4deg)' },
+          '40%': { transform: 'rotate(-2deg)' },
+          '50%': { transform: 'rotate(2.0deg)' },
+          '60%': { transform: 'rotate(0.0deg)' },
+          '100%': { transform: 'rotate(0.0deg)' },
+        },
+      },
+      animation: {
+        'star-animation': 'starAnimation 2s linear infinite',
+      },
   }
   },
   plugins: [],
