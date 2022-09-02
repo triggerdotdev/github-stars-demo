1a7bdb377e69fa18dd70ebc49e9566c49d5dcf7c
D-K-P <dkp.github@pm.me>
2022-09-02 11:24:07 +0100

    Initial layout for Star Count

--- github-stars-demo/pages/components/StarCount.tsx
+++ github-stars-demo/pages/components/StarCount.tsx
@@ -1,3 +1,11 @@
 export function StarCount() {
-  return <div className="w-96 h-96 bg-red-500 z-50 mb-10"></div>;
+  return (
+    <div className="flex flex-col items-center justify-top w-96 h-96 bg-red-500 z-50 mb-10 pt-10">
+      <h1 className="text-2xl font-bold font-poppins text-slate-100 mb-5">
+        'Repo name'
+      </h1>
+      <div className="w-48 h-48 bg-black z-50 mb-5"></div>
+      <h1 className="text-4xl font-bold font-poppins text-slate-100">XXX!</h1>
+    </div>
+  );
 }
