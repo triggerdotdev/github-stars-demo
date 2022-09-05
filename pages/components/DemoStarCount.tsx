b0022ee835ba8a18e17cd34e03602c565913acfa
D-K-P <dkp.github@pm.me>
2022-09-05 09:59:19 +0100

    Made the star bigger and swapped the title round

--- pages/components/DemoStarCount.tsx
+++ pages/components/DemoStarCount.tsx
@@ -16,19 +16,19 @@ export function DemoStarCount({
           className="animate-star-animation "
           src="/star-large.svg"
           alt="Star"
-          width="192"
-          height="192"
+          width="250"
+          height="250"
         />
       </div>
 
       {/* <p>"Loading..."</p> */}
       {/* <p>Something went wrong: we couldn't find the repo </p> */}
-      <h2 className="text-3xl font-bold font-poppins text-slate-100 mb-">
-        4,023 stars
-      </h2>
-      <p className="text-xl font-poppins text-slate-100 mb-5">
+      <p className="text-2xl font-poppins text-slate-100 mb-2">
         apihero-run/jsonhero-web{" "}
       </p>
+      <h2 className="text-3xl font-bold font-poppins text-slate-100">
+        4,023 GitHub stars
+      </h2>
     </>
   );
 }
