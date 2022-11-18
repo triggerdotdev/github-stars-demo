afeaf4951a58dd8d35b48da22720304be903a445
James Ritchie <james@jamesritchie.co.uk>
2022-11-18 12:40:38 +0000

    Added API Hero v2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+async function initProxy() {
+  const projectKey = "clame93mz0004h8fawc876xhq";
+  if (typeof window !== "undefined") {
+    const { setupWorker } = await import("apihero-js");
+    //update the allow list with the APIs you're using
+    await setupWorker({
+      allow: ["https://gateway.apihero.run/*"],
+      url: "https://proxy-staging.apihero.run",
+      projectKey,
+      env: process.env.NODE_ENV,
+    }).start();
+  } else {
+    const { setupProxy } = await import("apihero-js/node");
+    await setupProxy({
+      projectKey,
+      env: process.env.NODE_ENV,
+      url: "https://proxy-staging.apihero.run",
+    }).start();
+  }
+}
+initProxy();
+
+export default MyApp;
